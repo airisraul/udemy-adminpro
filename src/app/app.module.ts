12c03a62a9ca19db6f91b23218d987073e51f642
Raul <r.vargas@tesipro.com>
2020-04-20 10:11:20 +0200

    Fin de la seccion 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,16 @@ import { APP_ROUTES } from './app.routes';
 // Modulos
 import { PagesModule } from './pages/pages.module';
 
+// Servicios
+import { SettingsService } from './services/settings/settings.service';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+
+// Servicios
+import { ServiceModule } from './services/service.module';
 
 
 @NgModule({
@@ -23,9 +30,10 @@ import { RegisterComponent } from './login/register.component';
     BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
-  providers: [],
+  providers: [SettingsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SidebarService } from '../../services/service.index';
+
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -8,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor( public _sidebar: SidebarService ) { }
 
   ngOnInit(): void {
   }
