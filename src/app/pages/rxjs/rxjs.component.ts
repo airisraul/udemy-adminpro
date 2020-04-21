af514eb3db497b822c9fad80865b2fea1019554d
Raul <r.vargas@tesipro.com>
2020-04-21 16:33:54 +0200

    Seccion 8 terminada

---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit,OnDestroy } from '@angular/core';
+import { Observable, observable, Subscriber, Subscription } from 'rxjs';
+import { retry, map, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: [
+  ]
+})
+export class RxjsComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription;
+
+  constructor() {
+
+    this.subscription = this.regresaObservable()
+    .subscribe(
+      numero => console.log( 'Subs ', numero),
+      error => console.error('Error en el obs', error),
+      () => console.log('El observador termino')
+    );
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+    console.log('La página se va a cerrar');
+  }
+
+  regresaObservable(): Observable<any> {
+    return  new Observable( (observer: Subscriber<any>) => {
+
+      let contador = 0;
+
+      let intervalo = setInterval( () => {
+
+        contador += 1;
+
+        const salida = {
+          valor: contador
+        }
+
+        observer.next( salida) ;
+
+        // if (contador === 3) {
+        //   clearInterval( intervalo);
+        //   observer.complete();
+        // }
+
+        // if  (contador === 2){
+        //   // clearInterval( intervalo);
+        //   observer.error('auxilio!')
+        // }
+
+      }, 1000 );
+
+    }).pipe(
+      map( resp => resp.valor),
+      filter( ( valor, index ) => {
+
+        if ( ( valor % 2) === 1 ){
+          // impar
+          return true;
+        }else{
+          // par
+          return false;
+        }
+        // console.log('Filter',valor, index);
+        return true;
+      })
+          );
+  }
+}
